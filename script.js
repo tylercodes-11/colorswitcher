da279eaeaa90384aa5853b436c3b96fa46346532
Ty Reid <trawx11@gmail.com>
2022-06-06 11:24:31 -0400

    adding comment to scropt

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const colorSwitch = $('.colors li');//button
 colorSwitch.on ('click', function() {
     $('.colors > li').removeClass('active-color');
     $(this).addClass('active-color');
-    let newColor = $(this).attr('data-color');
-    $('body').css('background-color', newColor);
+    let newColor = $(this).attr('data-color'); //NEW COLORS HEX CODE
+    $('body').css('background-color', newColor); //CHANGING BACKGROUND
 });
