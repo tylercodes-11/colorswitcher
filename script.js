3dc596513ea1c913da63492e58fd39ad392e5aba
Ty Reid <trawx11@gmail.com>
2022-07-12 19:25:38 -0400

    active class comment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const colorSwitch = $('.colors li');//button
 
 colorSwitch.on ('click', function() {
     $('.colors > li').removeClass('active-color');
-    $(this).addClass('active-color');
+    $(this).addClass('active-color'); // new active background color
     let newColor = $(this).attr('data-color'); //NEW COLORS HEX CODE
     $('body').css('background-color', newColor); //CHANGING BACKGROUND
 });
